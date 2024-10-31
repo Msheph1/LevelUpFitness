2e187666b16336eba2732579aa754a476fe01c85
Msheph1 <max.r.shep@gmail.com>
2024-10-30 19:59:29 -0500

    started journal

---
+++ Frontend/LevelUpFitness/app/Journal/index.tsx
@@ -0,0 +1,14 @@
+import { Button, Text, TextInput, View, StyleSheet } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>testesdfdsi</Text>
+      <View style={{ width: 200 }}>
+        <Button title="+"></Button>
+      </View>
+    </View>
+  );
+};
+
+export default Index;
