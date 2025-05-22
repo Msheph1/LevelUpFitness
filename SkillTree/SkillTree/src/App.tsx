6efb64bb57004fc5b061e9ebf95b8fa9594a736f
Msheph1 <max.r.shep@gmail.com>
2025-05-21 20:19:45 -0500

    draggable working area

--- SkillTree/SkillTree/src/App.tsx
+++ SkillTree/SkillTree/src/App.tsx
@@ -6,21 +6,53 @@ import Exercise from "./assets/Tile/Exercise";
 function App() {
   const [count, setCount] = useState(0);
 
+  //logic for dragging screen
+  const box = document.querySelector<HTMLElement>(".dragablearea");
+  console.log(box);
+  console.log("refresh");
+  let isDown = false;
+  let startX = 0;
+  let startY = 0;
+  let scrollLeft = 0;
+  let scrollTop = 0;
+  if (box) {
+    box.addEventListener("mousedown", (e: MouseEvent) => {
+      isDown = true;
+      box.classList.add("active");
+      startX = e.pageX - box.offsetLeft;
+      startY = e.pageY - box.offsetTop;
+      scrollLeft = box.scrollLeft;
+      scrollTop = box.scrollTop;
+      box.style.cursor = "grabbing";
+    });
+
+    box.addEventListener("mouseleave", () => {
+      isDown = false;
+      box.style.cursor = "grab";
+    });
+
+    box.addEventListener("mouseup", () => {
+      isDown = false;
+      box.style.cursor = "grab";
+    });
+
+    box.addEventListener("mousemove", (e: MouseEvent) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - box.offsetLeft;
+      const y = e.pageY - box.offsetTop;
+      const walkX = x - startX;
+      const walkY = y - startY;
+      box.scrollLeft = scrollLeft - walkX;
+      box.scrollTop = scrollTop - walkY;
+    });
+  }
+
   return (
     <>
-      <h1>Skill Tree</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div style={{ display: "flex" }}>
-        <Exercise />
-        <Exercise></Exercise>
-      </div>
-
       <div className="dragablearea">
         <div className="innerarea">
+          <h1>Skill Tee</h1>
           <h1>title blah</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id,
@@ -31,6 +63,14 @@ function App() {
           <button>buton 1</button>
           <button>button 2</button>
           <p>morestaetsa seal;krejfsapokfjdsapoifjdsapoifjds</p>
+          <div>
+            <Exercise />
+            <Exercise />
+            <Exercise />
+            <Exercise />
+            <Exercise />
+            <Exercise />
+          </div>
         </div>
       </div>
     </>
